23d35e022d5c0e84290e0d60416a1a3b54459237
Daisy Leung <leungtszkuk@gmail.com>
2020-06-01 12:29:08 -0400

    init

---
+++ scrape_helper.py
@@ -0,0 +1,21 @@
+import os, glob
+import subprocess
+
+def file_len(fname):
+    p = subprocess.Popen(['wc', '-l', fname], stdout=subprocess.PIPE,
+                                              stderr=subprocess.PIPE)
+    result, err = p.communicate()
+    if p.returncode != 0:
+        raise IOError(err)
+    return int(result.strip().split()[0])
+
+if __name__ == '__main__':
+    review_files = glob.glob('scraped_data/*_2020*.csv')
+
+    x = 0
+    for filename in review_files:
+        x += file_len(filename)
+
+    print("totel reviews scraped, some may be duplicate: ", x)
+
+
