ce22a9b202dbeb7ba89fb0f8b48bf63c1005c542
Daisy Leung <leungtszkuk@gmail.com>
2020-06-10 18:04:55 -0400

    added T5

--- webapp/NLP_summarization.py
+++ webapp/NLP_summarization.py
@@ -1,4 +1,4 @@
-# NLP
+# NLPf
 import re
 import string
 import pandas as pd
@@ -11,7 +11,7 @@ from gensim.summarization import summarize, keywords
 from pprint import pprint
 
 
-def summarize_this_review(review):
+def extractive_sum(review):
 
     summary = summarize(review, ratio=0.2)  # word_count=20)    # max words
     kw = keywords(review)
@@ -24,3 +24,35 @@ def clean_special_char(df, text_field):
     df[text_field] = df[text_field].str.replace(r"\n", " ")
     return df
 
+
+def abstractive_sum(review, num_beams=4, no_repeat_ngram_size=2,
+                    min_length=30,
+                    max_length=200,
+                    early_stopping=True):
+    import torch
+    import json
+    from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
+
+    model = T5ForConditionalGeneration.from_pretrained('t5-small')
+    tokenizer = T5Tokenizer.from_pretrained('t5-small')
+    device = torch.device('cpu')
+
+    preprocess_text = review.strip().replace("\n", "")
+    t5_prepared_Text = "summarize: " + preprocess_text
+    print("original text preprocessed: \n", preprocess_text)
+
+    tokenized_text = tokenizer.encode(t5_prepared_Text,
+                                      return_tensors="pt").to(device)
+
+    # summmarize
+    summary_ids = model.generate(tokenized_text,
+                                 num_beams=num_beams,
+                                 no_repeat_ngram_size=no_repeat_ngram_size,
+                                 min_length=min_length,
+                                 max_length=max_length,
+                                 early_stopping=early_stopping)
+
+    output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
+    # print ("\n\nSummarized text: \n",output)
+    return output
